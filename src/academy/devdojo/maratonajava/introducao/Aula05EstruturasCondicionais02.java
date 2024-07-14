69e40599fefd4a996d38f8efef3c51fcaa1f4d9b
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-14 15:47:55 -0300

    Feat:Adicionado método para Ver o valor de imposto dependendo do seu salário

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -3,19 +3,19 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula05EstruturasCondicionais02 {
 
     public static void main(String[] args) {
-        double salarioAnual = 4500;
+        double salarioAnual = 7000;
         double primeiraFaixa = 9.70 / 100;
         double segundaFaixa = 37.36 / 100;
         double terceiraFaixa = 49.50 / 100;
         double valorImposto;
 
         if (salarioAnual <= 34712) {
-            salarioAnual = salarioAnual * primeiraFaixa;
+            valorImposto = salarioAnual * primeiraFaixa;
         } else if (salarioAnual >= 34712 && salarioAnual <= 68587) {
-            salarioAnual = salarioAnual * segundaFaixa;
+            valorImposto = salarioAnual * segundaFaixa;
         } else {
-            salarioAnual = salarioAnual * terceiraFaixa;
+            valorImposto = salarioAnual * terceiraFaixa;
         }
-        System.out.println(salarioAnual);
+        System.out.println(valorImposto);
     }
 }
